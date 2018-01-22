3d717e346abfb6b37981b2e6ea100e9108fba4aa
CarlosRueda48 <carlosruedag48@gmail.com>
2018-01-22 16:59:04 -0600

    Finished Anagram Mappings code and README

--- katas/kata1/anagrammappings.c
+++ katas/kata1/anagrammappings.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * Return an array of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
@@ -9,7 +9,6 @@ int* anagramMappings(int* A, int ASize, int* B, int BSize, int returnSize) {
   for(int i=0;i<ASize;i++){
       for(int j=0;j<BSize;j++){
           if(A[i]==B[j]){
-            fprintf("Number %i matches on position %i",i,j);
             anagram[i] = j;
             break;
           }
@@ -19,7 +18,11 @@ int* anagramMappings(int* A, int ASize, int* B, int BSize, int returnSize) {
 }
 
 int main(){
-  int[] A = [12, 28, 46, 32, 50];
-  int[] B = [50, 12, 32, 46, 28];
-  anagramMappings(A,,sizeof(A),B,sizeof(B));
+  int A[] = {12, 28, 46, 32, 50};
+  int B[] = {50, 12, 32, 46, 28};
+  int* result = anagramMappings(A,sizeof(A),B,sizeof(B),sizeof(A));
+  for(int i = 0; i < sizeof(result); i++){
+    printf("Number %d on position %d in array A matches the number on position %d in array B \n", A[i], i, result[i]);
+  }
+  printf("%d",result[0]);
 }
