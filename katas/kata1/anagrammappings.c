667728cf3063ae39bc5ed89119441bc3cb610b58
CarlosRueda <carlosruedag48@gmail.com>
2018-01-22 14:17:15 -0600

    Tried some bugfixing

--- katas/kata1/anagrammappings.c
+++ katas/kata1/anagrammappings.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
 int* anagramMappings(int* A, int ASize, int* B, int BSize, int returnSize) {
   int* anagram = malloc(sizeof(int)*returnSize);
   for(int i=0;i<ASize;i++){
       for(int j=0;j<BSize;j++){
-          if(a[i]==b[j]){
+          if(A[i]==B[j]){
+            fprintf("Number %i matches on position %i",i,j);
             anagram[i] = j;
             break;
           }
@@ -14,7 +19,7 @@ int* anagramMappings(int* A, int ASize, int* B, int BSize, int returnSize) {
 }
 
 int main(){
-  int[] A = [12, 28, 46, 32, 50]
-  int[] B = [50, 12, 32, 46, 28]
-  intanagram(A,B,sizeof(A));
+  int[] A = [12, 28, 46, 32, 50];
+  int[] B = [50, 12, 32, 46, 28];
+  anagramMappings(A,,sizeof(A),B,sizeof(B));
 }
